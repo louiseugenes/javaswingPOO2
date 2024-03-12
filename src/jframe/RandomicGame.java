b4de22b9ee853bc4815e51ae546adbff7ecbe3f6
Luis Eugênio <123472957+louiseugenes@users.noreply.github.com>
2024-03-12 20:42:23 -0300

    Precisa fazer Fix do verifyButton e do tryAgain
    Ocorre um erro ao quando eu executo a função para alterar a visibilidade desses botões no código, apenas o textField consigo alterar:
    o erro:
    Exception in thread "AWT-EventQueue-0" java.lang.NullPointerException: Cannot invoke "javax.swing.JButton.setVisible(boolean)" because "this.verifyButton" is null

---
+++ src/jframe/RandomicGame.java
@@ -0,0 +1,125 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java. util. Random;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RandomicGame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField numberChoiceField;
+	private JButton verifyButton;
+	private JButton tryAgainButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RandomicGame frame = new RandomicGame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RandomicGame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 342, 233);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel numberTitleLabel = new JLabel("<html><h1>Advinhe o Número</h1><html>");
+		numberTitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		numberTitleLabel.setBounds(53, 11, 230, 49);
+		contentPane.add(numberTitleLabel);
+		
+		JLabel numberSubtitleLabel = new JLabel("<html><h4>Digite o Número:</h4><html>");
+		numberSubtitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		numberSubtitleLabel.setBounds(100, 58, 132, 27);
+		contentPane.add(numberSubtitleLabel);
+		
+		numberChoiceField = new JTextField();
+		numberChoiceField.setBounds(78, 96, 175, 35);
+		contentPane.add(numberChoiceField);
+		numberChoiceField.setColumns(10);
+		
+		JButton verifyButton = new JButton("<html><h2><b>Arriscar</b></h2><html>");
+		verifyButton.setBounds(78, 142, 175, 35);
+		verifyButton.setVisible(true);
+		contentPane.add(verifyButton);
+		
+		verifyButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				verifySortNumber();
+			}
+		});
+		
+		JButton tryAgainButton = new JButton("<html><h2><b>Tente Novamente</b></h2><html>");
+		tryAgainButton.setBounds(78, 142, 175, 35);
+		tryAgainButton.setVisible(false);
+		contentPane.add(tryAgainButton);
+		
+		
+		tryAgainButton.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		        numberChoiceField.setVisible(true);
+		        verifyButton.setVisible(true);
+		        tryAgainButton.setVisible(false);
+		    }
+		});
+	}
+	
+	private void verifySortNumber() {
+
+    	try {
+    		Random randomNumber = new Random();
+    		String numberChoiced = numberChoiceField.getText();
+    		
+    		int sortedNumber = randomNumber.nextInt(100);
+    		int convertedToIntNumber = Integer.parseInt(numberChoiced);
+    		
+            if (convertedToIntNumber != 0) {
+            	if (sortedNumber != convertedToIntNumber) {
+            		JOptionPane.showMessageDialog(this, "Não foi dessa vez :/", "Loose", JOptionPane.ERROR_MESSAGE);
+            		//numberChoiceField.setVisible(false);
+            		verifyButton.setVisible(false);
+            		//tryAgainButton.setVisible(true);
+            	}
+            	else {
+            		JOptionPane.showMessageDialog(this, "Parabéns, você ganhou!! :D", "Win!!", JOptionPane.INFORMATION_MESSAGE);
+            		//numberChoiceField.setVisible(false);
+            		//verifyButton.setVisible(false);
+            		//tryAgainButton.setVisible(true);
+            	}
+
+            } else {
+                JOptionPane.showMessageDialog(this, "Por favor, digite o número!", "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(this, "Por favor, insira um valor numérico válido.", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
